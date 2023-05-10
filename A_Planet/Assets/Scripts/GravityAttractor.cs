a87acfa048396ea43077655bcae1e0475c8e43ae
Min100KM <min100kim@ewhain.net>
2023-05-10 16:05:53 +0900

    Planet_A

---
+++ A_Planet/Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAttractor : MonoBehaviour
+{
+    public float gravity = -10f;
+    // Start is called before the first frame update
+    
+    public void Attract(Transform body){
+        Vector3 targetDir = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.rotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.rotation;
+        body.GetComponent<Rigidbody>().AddForce(targetDir * gravity);
+        
+    }
+
+}
