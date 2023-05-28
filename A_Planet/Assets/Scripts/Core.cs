b0cad3a60b4551f75805c8f7562254d8dd4b1f49
jiuumm <129347578+jiuumm@users.noreply.github.com>
2023-05-28 20:30:16 +0900

    UI수정본
    토네이도 랜덤 이동 구현함 [근데 플레이어쪽으로 안옴 ..ㄷㄷ]
    플레이어의 HP시스템
    Left Controller 위에 hp canvas를 배치!!
    시야에 오면 2초 지나면 UI가 왼손 컨트롤러 위에 뜬다. 시야에서 벗어나면 hp canvas사라짐
    HP- 구현완료
    토네이도에 부딪치면 캔버스의 hp가 줄어들기 구현 함
    script: Core
    손 model 추가완료
    Survival Time - 구현 완료
    게임이 시작되면 생존시간이 information canvas에 실시간으로 나타난다!
    게임 start버튼 - 구현완료
    저 흰색 배너같은 것을 레이로 가리키면 게임이 시작되도록 만듦.
    Tracked Device Graphic Raycaster이용하면 가능!
    이 컴포넌트를 해당 배너 자식에 넣으면 됨[노가다하다가 발견..]
    +Button 컴포넌트도 추가해서 On Click()에 start - eventBridge추가

---
+++ A_Planet/Assets/Scripts/Core.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Core : MonoBehaviour
+{
+
+    public int maxHp = 100;
+    private int hp;
+
+    public UnityEvent<string> onHpChanged;
+    public UnityEvent OnHit;
+    public UnityEvent OnDestroy;
+
+    private static Core instance;
+    public static Core Instance
+    {
+
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType<Core>();
+                return instance;
+            
+        }
+    }
+    private void Awake()
+    {
+        instance = this;        
+    }
+    private void OnEnable()
+    {
+        hp = maxHp;
+        UpDateUI();
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        var tornado = other.GetComponent<TornadoRandomMove>();
+        if(tornado != null)
+        {
+            OnHit?.Invoke();
+            DecreaseHP(1);
+        }
+    }
+
+    private void DecreaseHP(int amount)
+    {
+        if (hp <= 0)
+        {
+            return;
+          
+        }
+        hp -= amount;
+        if (hp <= 0)
+        {
+            hp = 0;
+            OnDestroy?.Invoke();
+        }
+        UpDateUI();
+    }
+
+    private void UpDateUI()
+    {
+        onHpChanged?.Invoke($"HP: {hp}");
+    }
+}
